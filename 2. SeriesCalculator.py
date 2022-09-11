4bff58b6e90ab9b3d5179dd452d3603aa6392a6d
Dev_DigAll <34727855+Seon-e3@users.noreply.github.com>
2022-09-11 13:33:52 +0900

    Update 2. SeriesCalculator.py

--- 2. SeriesCalculator.py
+++ 2. SeriesCalculator.py
@@ -2,6 +2,7 @@ number = int(input("밑을 입력하세요> "))
 exponent = int(input("지수를 입력하세요> "))
 result = list()
 
+
 for i in range(1, exponent + 1):
     print(int(number), "^", i, "=", int(number)**i)
     result.append(int(number)**i)
